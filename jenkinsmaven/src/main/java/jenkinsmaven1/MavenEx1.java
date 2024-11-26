90f1360fd31aa4dd272c8c5d9c7519958b5255cd
Aiswarya-007 <aiswaryajithin25@gmail.com>
2024-11-26 12:37:12 +0530

    jenkins

---
+++ jenkinsmaven/src/main/java/jenkinsmaven1/MavenEx1.java
@@ -0,0 +1,11 @@
+package jenkinsmaven1;
+
+public class MavenEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+
+	}
+
+}
