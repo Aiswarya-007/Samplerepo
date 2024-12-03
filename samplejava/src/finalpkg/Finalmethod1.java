172afe64a96e12974e510fdc9d0e075e5604477b
Aiswarya-007 <aiswaryajithin25@gmail.com>
2024-12-03 14:36:30 +0530

    intial commit 1

---
+++ samplejava/src/finalpkg/Finalmethod1.java
@@ -0,0 +1,9 @@
+package finalpkg;
+
+public class Finalmethod1 
+{
+	public final void display()
+	{
+		System.out.println("This is a final method");
+	}
+}
---
+++ samplejava/src/finalpkg/Finalmethod2.java
@@ -0,0 +1,16 @@
+package finalpkg;
+
+public class Finalmethod2 extends Finalmethod1
+{
+
+	public static void main(String[] args) 
+	{
+		
+	}
+	
+	//public void display()
+	{
+		//cannot override final method
+	}
+
+}
